2fa32f05a9f0a241b0343610c116f45c7740c564
mssffct <herooftheday82@mail.ru>
2020-04-23 14:27:13 +0400

    slick-slider added

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1500,
+        adaptiveHeight: true,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
