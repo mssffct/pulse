8ee0fa98c1667d21da7cc1603ac9c6add28126ac
mssffct <herooftheday82@mail.ru>
2020-04-27 16:34:29 +0400

    tabs and tab scripts added

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // скрипт для карусели
     $('.carousel__inner').slick({
         speed: 1500,
         adaptiveHeight: true,
@@ -16,4 +17,23 @@ $(document).ready(function(){
             }
         ]
     });
+    // скрипт для табов
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+    // скрипт для кнопки "подробнее" в табах
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        })
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
