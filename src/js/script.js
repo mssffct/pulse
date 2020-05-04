81761479671c2701589a7892cd4d108fbc775593
mssffct <herooftheday82@mail.ru>
2020-05-04 18:58:37 +0300

    modal windows added

--- src/js/script.js
+++ src/js/script.js
@@ -36,8 +36,8 @@ $(document).ready(function(){
     // скрипт для табов
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
     // скрипт для кнопки "подробнее" в табах
     function toggleSlide(item) {
@@ -52,4 +52,21 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // modal windows
+    // выводим окно консультации при нажатии на кнопки с дата-аттрибутом consultation
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    // код для закрытия окон при нажатии на крестик
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+    // открываем окно заказа после нажатия на кнопку покупки. выводится название выбраного товара
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
 });
