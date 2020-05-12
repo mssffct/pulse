18a19cd29b2caba6549341eb8da620112db230fa
mssffct <herooftheday82@mail.ru>
2020-05-12 15:31:35 +0400

    animation added, some promblems with emailer solved

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,9 @@
 $(document).ready(function(){
-    // скрипт для карусели
     $('.carousel__inner').slick({
-        speed: 1500,
+        speed: 1200,
         adaptiveHeight: true,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
@@ -14,32 +11,16 @@ $(document).ready(function(){
                     dots: true,
                     arrows: false
                 }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    dots: false,
-                    arrows: false
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    dots: false,
-                    arrows: false
-                }
-            },
+            }
         ]
     });
-    // скрипт для табов
+    
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
-    // скрипт для кнопки "подробнее" в табах
+
     function toggleSlide(item) {
         $(item).each(function(i) {
             $(this).on('click', function(e) {
@@ -47,37 +28,34 @@ $(document).ready(function(){
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
             })
-        })
+        });
     };
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
-    // modal windows
-    // выводим окно консультации при нажатии на кнопки с дата-аттрибутом consultation
-    $('[data-modal=consultation]').on('click', function () {
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function() {
         $('.overlay, #consultation').fadeIn('slow');
     });
-    // код для закрытия окон при нажатии на крестик
-    $('.modal__close').on('click', function () {
-        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
     });
-    // открываем окно заказа после нажатия на кнопку покупки. выводится название выбраного товара
+
     $('.button_mini').each(function(i) {
-        $(this).on('click', function(){
+        $(this).on('click', function() {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
         })
     });
 
-    // валидация форм jquery
-
-    function validateForms(form) {
+    function validateForms(form){
         $(form).validate({
             rules: {
                 name: {
                     required: true,
-                    minlenght: 2
+                    minlength: 2
                 },
                 phone: "required",
                 email: {
@@ -88,12 +66,12 @@ $(document).ready(function(){
             messages: {
                 name: {
                     required: "Пожалуйста, введите свое имя",
-                    minlenght: jQuery.validator.format("Введите не менее {0} символов!")
+                    minlength: jQuery.validator.format("Введите {0} символа!")
                 },
                 phone: "Пожалуйста, введите свой номер телефона",
                 email: {
                     required: "Пожалуйста, введите свою почту",
-                    email: "Неправильно введен почтовый адрес"
+                    email: "Неправильно введен адрес почты"
                 }
             }
         });
@@ -102,10 +80,9 @@ $(document).ready(function(){
     validateForms('#consultation-form');
     validateForms('#consultation form');
     validateForms('#order form');
-    // маска ввода номера (шаблон)
+
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
-    // отправка почты с сайта, файлы мейлера на пхп, там написать свой логин и пароль почты а так же в строке setForm
     $('form').submit(function(e) {
         e.preventDefault();
         $.ajax({
@@ -115,9 +92,29 @@ $(document).ready(function(){
         }).done(function() {
             $(this).find("input").val("");
             $('#consultation, #order').fadeOut();
-            $('.overlay, #thanks').fadeIn(slow);
+            $('.overlay, #thanks').fadeIn('slow');
+
             $('form').trigger('reset');
         });
         return false;
     });
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    // new WOW().init();
 });
+
